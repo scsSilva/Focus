ff522a0b239a1e335b3569e73b80814d75261043
Sallys Carlos <sallys.carlos93@gmail.com>
2023-09-03 14:11:53 -0300

    feat: history page

---
+++ src/Pages/History/index.tsx
@@ -0,0 +1,66 @@
+import * as Styles from "./styles";
+
+const History = () => {
+  return (
+    <Styles.Container>
+      <h1>Meu histórico</h1>
+
+      <Styles.List>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Styles.Status statusColor="green">Concluído</Styles.Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Styles.Status statusColor="green">Concluído</Styles.Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Styles.Status statusColor="green">Concluído</Styles.Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Styles.Status statusColor="yellow">Em andamento</Styles.Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Styles.Status statusColor="red">Interrompido</Styles.Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </Styles.List>
+    </Styles.Container>
+  );
+};
+
+export default History;
