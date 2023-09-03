910c2b7cb18e7b83746875862b3f489305723c6a
Sallys Carlos <sallys.carlos93@gmail.com>
2023-09-03 16:26:02 -0300

    chore: alter name of folder pages

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home";
-import History from "./Pages/History";
+import Home from "./pages/Home";
+import History from "./pages/History";
 import DefaultLayout from "./layouts/DefaultLayout";
 
 const Router = () => {
